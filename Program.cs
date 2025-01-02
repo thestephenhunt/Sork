dd89d314e9d046743bd42aec6f384a8454bf92e0
Stephen Hunt <stephen.hunt@improving.com>
2025-01-02 14:14:57 -0600

    Added dance, sing, and whistle

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@
             input = input.ToLower();
             input = input.Trim();
             if (input == "lol") { Console.WriteLine("You laugh out loud"); }
+            else if (input == "dance") { Console.WriteLine("You dance awkwardly"); }
+            else if (input == "sing") { Console.WriteLine("You sing off key"); }
+            else if (input == "whistle") { Console.WriteLine("You whistle loudly"); }
             else if (input == "exit") { break;}
             else { Console.WriteLine("Huh?"); }
         } while (true);
