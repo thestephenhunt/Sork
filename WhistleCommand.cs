d8302528a630c51aacfd66c641e828bbf3aafab4
Stephen Hunt <stephen.hunt@improving.com>
2025-01-03 14:11:08 -0600

    Lesson 2 homework

---
+++ WhistleCommand.cs
@@ -0,0 +1,9 @@
+public class WhistleCommand : ICommand
+{
+    public bool Handles(string userInput) => userInput == "whistle";
+    public CommandResult Execute()
+    {
+        Console.WriteLine("You whistle loudly");
+        return new CommandResult { RequestExit = false, IsHandled = true };
+    }
+}   
